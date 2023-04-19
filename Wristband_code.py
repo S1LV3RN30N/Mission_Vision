1857c637dbaa26714dae74e11015998d0e35d571
DarcBunny <130702965+DarcBunny@users.noreply.github.com>
2023-04-19 10:39:07 -0700

    Update Wristband_code.py

--- Wristband_code.py
+++ Wristband_code.py
@@ -1,4 +1,11 @@
 #This is where the code for the wristband goes
+#input/pre-video processing. subject to change
+#note to self: find out how to send video to the hub if it is needed
+
+
+
+
+#Output/post-video processing
 distance = 
 concern_distance = 
 output_no_concern = 
