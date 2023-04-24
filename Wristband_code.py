b0c1cf2937dad75149841b96d3700c0dcfb5787e
DarcBunny <130702965+DarcBunny@users.noreply.github.com>
2023-04-24 13:11:35 -0700

    Update Wristband_code.py

--- Wristband_code.py
+++ Wristband_code.py
@@ -1,17 +1,19 @@
 #This is where the code for the wristband goes
+#needed imports
+from gpiozero import DistanceSensor
 #input/pre-video processing. subject to change
 #note to self: find out how to send video to the hub if it is needed
+ultrasonic = DistanceSensor(echo=4, trigger=7)
+while true:
+  distance = ultrasonic.distance
+  concern_distance = 
+  output_no_concern = 
+  alert_distance = 
+  output_concern = 
+  output_alert = 
 
-
-
-
+  
 #Output/post-video processing
-distance = 
-concern_distance = 
-output_no_concern = 
-alert_distance = 
-output_concern = 
-output_alert = 
 while distance >= 0:
   if distance >= concern_distance:
     output_no_concern;
