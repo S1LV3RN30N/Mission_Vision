fc8a54823464776c9112d126223db28500fc583a
DarcBunny <130702965+DarcBunny@users.noreply.github.com>
2023-05-01 13:20:05 -0700

    Update Hub Processing Code.py

--- Hub Processing Code.py
+++ Hub Processing Code.py
@@ -1,3 +1,4 @@
 # Here is the hub code.
 # Note this is an upload test:
 # (This is for processing the code from the wristband so we can make the wristband: 1. Not take up as much battery 2. Having more freedom with the system CPU power and hopefully improving the efficiency.)
+#have green light pop on when recieving power, red flash when low battery or error
